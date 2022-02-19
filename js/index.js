5567162194f19e54db53c6dce8e710e4ade89507
Elena Kachemirova <kachemirovaelena@gmail.com>
2022-02-19 23:58:11 +0300

    First commit

---
+++ js/index.js
@@ -0,0 +1,4 @@
+document.getElementById('expand_products').addEventListener('click', function(event) {
+  event.target.style.display = 'none';
+  document.getElementById('collapsed_products').style.display = 'block';
+});
